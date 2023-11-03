2ce34b6d66815b5a557244c57d26e72f71d0e181
Jason Rowland <jay_r83@outlook.com>
2023-11-02 21:55:22 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    const cards = [];
+
+    for(let count = 0; count < array.length; count++){
+        cards.push(`Thank you, ${array[count]}, for the wonderful ${event} gift!`);
+    }
+
+    return cards;
+};
+
+function countDown(count){
+    while(count >= 0){
+        console.log(count)
+        count--;
+    }
+};
